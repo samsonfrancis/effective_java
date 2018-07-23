a2d238501708ad0272b555c0f672609b67a19c1b
samsonfrancis <samson@kritter.in>
2018-07-23 21:42:48 +0530

    defensive copy of objects

---
+++ src/main/java/com/sam/effective_java/defensive_copy_of_objects/DefensiveCopyOfObjects.java
@@ -0,0 +1,34 @@
+package com.sam.effective_java.defensive_copy_of_objects;
+
+import java.util.Date;
+
+/**
+ * This class demonstrates the use and necessity of defensive copy of the objects.
+ * <b>instead of passing Date object pass long millies which is more efficient and elegant.</b>
+ */
+public class DefensiveCopyOfObjects {
+    private Date startDate = null;
+    private Date endDate = null;
+
+    /**
+     * The constructor does not directly assign the values it creates a new object with the given values and
+     * assigns them to instance variables.
+     * @param startDate startDate not to be modified
+     * @param endDate end date not to be modified
+     */
+    public DefensiveCopyOfObjects(Date startDate, Date endDate) {
+        this.startDate = new Date(startDate.getTime());
+        this.endDate = new Date(endDate.getTime());
+    }
+
+    /**
+     * This constructor is better than the previous as it takes the primitive types as args
+     * this is very defensive compared to the previous one.
+     * @param startDateMillies
+     * @param endDateMillies
+     */
+    public DefensiveCopyOfObjects(long startDateMillies, long endDateMillies){
+        this.startDate = new Date(startDateMillies);
+        this.endDate = new Date(endDateMillies);
+    }
+}
